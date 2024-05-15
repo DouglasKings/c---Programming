bc28af146335a42bc6974e67b2b41e65ffd66451
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-05-15 23:23:35 +0300

    Create personalMessage.cpp

---
+++ personalMessage.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void personalMessages()
+{
+    cout << "I am good at C++." << endl;
+    cout << "Functions in C++ are awesome." << endl;
+    cout << "I am soon to be a pro at C++." << endl;
+}
+
+int main()
+{
+    personalMessages();
+
+    return 0;
+}
