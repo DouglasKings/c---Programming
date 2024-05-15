e433c1baa10fda266de44c65f39a2d240b9e6543
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-05-15 23:11:33 +0300

    Create integers.cpp

---
+++ integers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Num1, Num2;
+	cout << "Enter the integer: " << endl;
+	cin >> Num1;
+
+	cout << "Enter the integer: " << endl;
+	cin >> Num2;
+
+	int Sum = Num1 + Num2;
+	cout << Sum << " is the integer." << endl;
+
+	int Product = Num1 * Num2;
+	cout << Product << " is the integer." << endl;
+
+	return 0;
+}
