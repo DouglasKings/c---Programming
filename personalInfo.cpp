88d0bbe4d9a276d010d24ffd2ba4258308b439b6
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-05-15 23:16:11 +0300

    Create personalInfo.cpp

---
+++ personalInfo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string Greetings, Name, Interest;
+	double RollNumber;
+
+	cout << "Hey \n" << endl;
+
+	cout << "What's your name? " << endl;
+	cin >> Name;
+	cout << "My name is  " << " " << "Name" << endl;
+
+	cout << "What's your roll number? " << endl;
+	cin >> RollNumber;
+	cout << "My roll number is  " << " " << RollNumber << endl;
+
+	cout << "What's your field of interest? " << endl;
+	cin >> Interest;
+	cout << "My fields of interest are  " << " " << "Interest" << endl;
+
+	return 0;
+}
