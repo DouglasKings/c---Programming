b34d0e0078629564524bec517849433fe0d79e7e
Douglas Kings Kato <141626239+DouglasKings@users.noreply.github.com>
2024-05-15 23:10:13 +0300

    Create sum&Prod.cpp

---
+++ sum&Prod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Firstinteger;
+	cout << "Enter the first integer " << endl;
+	cin >> Firstinteger;
+
+	int Secondinteger;
+	cout << "Enter the second integer " << endl;
+	cin >> Secondinteger;
+
+	int Sum = Firstinteger + Secondinteger;
+	int Product = Firstinteger * Secondinteger;
+
+	cout << "Sum is " << " "<< Sum << endl;
+	cout << "Product is " << " " << Product << endl;
+
+	return 0;
+}
